dba291123d92dbefc88eb100530ba4d8acbddcc8
Mike Kunkel <mike@mkunkel.com>
2024-04-10 10:17:11 -0500

    Add cells project

---
+++ cells/lib/cells.ex
@@ -0,0 +1,18 @@
+defmodule Cells do
+  @moduledoc """
+  Documentation for `Cells`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cells.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
